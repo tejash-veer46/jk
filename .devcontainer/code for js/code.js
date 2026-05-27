d3e94b883e590557915e1893bc69acf18da1f534
tejash-veer46 <158097277+tejash-veer46@users.noreply.github.com>
2026-05-27 18:25:24 +0000

    new code

--- .devcontainer/code for js/code.js
+++ .devcontainer/code for js/code.js
@@ -4,3 +4,9 @@ console.log("second commit is verifed now on profile")
 console.log("made changes on the UI")
 console.log("MADE CHANGES TO THE BACKEND")
 console.log("BOTH BACKEND AND UI WORKING VERIFIED")
+
+
+const original = "Hello World";
+const reversed: string = original.split("").reverse().join("");
+
+console.log(reversed); // Output: "dlroW olleH"
